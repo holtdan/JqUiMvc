481dc1bb0fb1113f15813cc25f449ae1ca14dd71
Daniel Holt <dholt@briefingedge.com>
2014-06-30 23:07:54 -0400

    Missed this file!

---
+++ JqUiMvc/Models/VisitViewModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JqUiMvc.Models
+{
+    public class VisitViewModelBase
+    {
+        public StepState StepState { get; set; }
+    }
+}
