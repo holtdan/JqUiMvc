8636395f8b700347e8db7388914789670fb79548
Daniel Holt <dholt@briefingedge.com>
2014-07-24 12:45:57 -0400

    Db1st core

---
+++ BEDataAccess/BEDataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEDataAccess
+{
+    class BEDataContext : DbContext
+    {
+        public DbSet<Site> Sites { get; set; }
+    }
+}
---
+++ JqUiMvc.Tests/Data/DbEngineDriver.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JqUiMvc.Tests.Data
+{
+    [TestClass]
+    public class DbEngineDriver
+    {
+        [TestMethod]
+        public void First()
+        {
+            var e = new BEDataAccess.Engine();
+        }
+    }
+}
